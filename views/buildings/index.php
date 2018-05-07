182cc2abf768d02ddbf8bdd300e463985c9e3acf
Ponych Vlad <vladyclavhello@gmail.com>
2018-05-07 15:40:40 +0300

    Update buildings page/index and style for it

--- views/buildings/index.php
+++ views/buildings/index.php
@@ -4,22 +4,34 @@
         <div class="container">
             <div class="mdl-tabs mdl-js-tabs mdl-js-ripple-effect">
                 <div class="mdl-tabs__tab-bar">
-                    <a href="#starks-panel" class="mdl-tabs__tab is-active">Список всех культурных сооружений</a>
+                    <a href="#all-buildings" class="mdl-tabs__tab is-active">Список всех культурных сооружений</a>
                     <a href="#lannisters-panel" class="mdl-tabs__tab">Lannisters</a>
                     <a href="#targaryens-panel" class="mdl-tabs__tab">Targaryens</a>
                 </div>
                 <!--                First TAB-->
-                <div class="mdl-tabs__panel is-active" id="starks-panel">
+                <div class="mdl-tabs__panel is-active" id="all-buildings">
                     <div class="row">
                         <? foreach ($listBuildings as $list): ?>
+                            <?
+                            $descripton = substr($list['discription'], 0, 400);
+                            $descripton = rtrim($descripton, "!,.-");
+                            $descripton = substr($descripton, 0, strrpos($descripton, ' '));
+                            ?>
                             <section id="building-main-card" class="section--center mdl-grid mdl-grid--no-spacing mdl-shadow--2dp">
-                                <div class="mdl-card mdl-cell mdl-cell--12-col" >
-                                    <div class="mdl-card__supporting-text">
-                                        <h4>Technology</h4>
-                                        Dolore ex deserunt aute fugiat aute nulla ea sunt aliqua nisi cupidatat eu. Nostrud in laboris labore nisi amet do dolor eu fugiat consectetur elit cillum esse. Pariatur occaecat nisi laboris tempor laboris eiusmod qui id Lorem esse commodo in. Exercitation aute dolore deserunt culpa consequat elit labore incididunt elit anim.
+                                <div id="building-card-content" class="mdl-card mdl-cell mdl-cell--12-col" style="background-image: url('../template/media/images/cultural/<?= $list['title_img'] ?>');">
+                                    <div id="building-support-text" class="mdl-card__supporting-text">
+                                        <h4><?
+                                            if ($list['type_of_building'] == 'cinema'){
+                                                echo "кинотеатр " . $list['name'];
+                                            }
+                                            elseif ($list['type_of_building'] == 'theatre'){
+                                                echo "театр " . $list['name'];
+                                            }
+                                            ?></h4>
+                                        <?=$descripton. '... '?>
                                     </div>
                                     <div id="building-actions-border" class="mdl-card__actions">
-                                        <a href="actor/<?=$list['id']?>" class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                                        <a href="cultural/<?=$list['id']?>" class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect color-accent">
                                             Подробнее
                                         </a>
                                     </div>
@@ -27,10 +39,10 @@
                                 <button class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="building-btn">
                                     <i class="material-icons">more_vert</i>
                                 </button>
-                                <ul class="mdl-menu mdl-js-menu mdl-menu--bottom-left" for="building-btn">
-                                    <li class="mdl-menu__item">Lorem</li>
-                                    <li class="mdl-menu__item" disabled>Ipsum</li>
-                                    <li class="mdl-menu__item">Dolor</li>
+                                <ul class="mdl-menu mdl-js-menu mdl-menu--bottom-right" for="building-btn">
+                                    <li id-actor="<?=$list['id']?>" class="mdl-menu__item edit-actor"><a
+                                                href="/actors/update/<?=$list['id']?>">Редактировать даные</a></li>
+                                    <li id-actor="<?=$list['id']?>" class="mdl-menu__item delete-actor">Удалить из списка</li>
                                 </ul>
                             </section>
                         <? endforeach; ?>
